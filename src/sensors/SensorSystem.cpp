482abfe8aad039ce764403ea7be315c5b2eb9167
Igor Brandão <igorabrandao@gmail.com>
2026-05-19 13:01:13 +0200

    Implement sensor packet handling in SensorSystem update method. Added functionality to receive and process SensorPacket data, converting it to a normalized quaternion for orientation management.

--- src/sensors/SensorSystem.cpp
+++ src/sensors/SensorSystem.cpp
@@ -1,4 +1,6 @@
 #include "SensorSystem.h"
+#include "networking/SensorPacket.h"
+#include "networking/UdpReceiver.h"
 
 namespace sensors {
 
@@ -26,7 +28,20 @@ const glm::quat &SensorSystem::getOrientation() const { return orientation_; }
  * @brief Update the sensor system
  */
 void SensorSystem::update() {
-  // TODO
+  // Temporary packet
+  SensorPacket packet;
+
+  // Get the latest packet from the receiver
+  if (!receiver_.getLatestPacket(packet)) {
+    return;
+  }
+
+  // Convert the packet to a glm::quat
+  // The quaternion is in the order of w, x, y, z
+  glm::quat orientation = glm::quat(packet.qw, packet.qx, packet.qy, packet.qz);
+
+  // Normalize and store the orientation
+  orientation_ = glm::normalize(orientation);
 }
 
 } // namespace sensors
